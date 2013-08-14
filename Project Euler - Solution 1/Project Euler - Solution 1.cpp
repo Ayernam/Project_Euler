d29ebbf9cafd9dc1d245e1a0dfd056787de8ace2
Amir Amhaz <namhaza4@outlook.com>
2013-08-14 14:13:03 -0400

    Problem 1 completed

--- Project Euler - Solution 1/Project Euler - Solution 1.cpp
+++ Project Euler - Solution 1/Project Euler - Solution 1.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 int main(){
-
+    int sum=0, n=0;
+    while(n<1000){
+        if(n%3==0 || n%5==0){
+            sum+=n;
+        }
+        n++;
+    }
+    cout << sum << endl;
+    system("PAUSE");
+    return 0;
 }
